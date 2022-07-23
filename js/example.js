08022c9af2583252f84e60b2b690ef939f09da6b
justyoonjh <juhoyoon0901@naver.com>
2022-07-23 22:18:18 +0900

    start

---
+++ js/example.js
@@ -0,0 +1,14 @@
+    // const url = 'http://localhost3000/test';
+    // const options = {
+    //     method: 'POST',
+    //     header: {
+    //         'Accept': 'application/json',
+    //         'Content-Type': 'application/jsoncharset=UTP-8'
+    //     },
+    //     body: JSON.stringify({
+    //         name: 'Juho',
+    //         age: 20
+    //     })
+    // }
+    // fetch(url, options)
+    //     .then(response => console.log(response));

62b8b90a7953c42383c5438903d8d438f841e451
justyoonjh <juhoyoon0901@naver.com>
2022-07-24 16:25:35 +0900

    Yunflix-prototype

--- js/movie.js
+++ js/movie.js
@@ -1,10 +1,10 @@
 const APIKEY = "5e5e4c75082981b17a79210ea2780821";
+const ImgURL = "https://image.tmdb.org/t/p/w500/";
+
+
+
 
 const infoURL = "https://api.themoviedb.org/3/movie/now_playing?api_key=5e5e4c75082981b17a79210ea2780821&language=en-US&page=1";
-
-
-
-
 fetch(infoURL)
     .then(response => response.json())
     .then(response => { console.log(response) });

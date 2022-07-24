2a1c3d4b40bc99506a24b0149e406b6ce154e96f
justyoonjh <juhoyoon0901@naver.com>
2022-07-24 14:12:15 +0900

    movieworld-ver1

--- js/movie.js
+++ js/movie.js
@@ -1,6 +1,10 @@
 const APIKEY = "5e5e4c75082981b17a79210ea2780821";
 
 const infoURL = "https://api.themoviedb.org/3/movie/now_playing?api_key=5e5e4c75082981b17a79210ea2780821&language=en-US&page=1";
+
+
+
+
 fetch(infoURL)
     .then(response => response.json())
     .then(response => { console.log(response) });
